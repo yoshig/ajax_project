641e4dc84621ed237eb91c98de009712340f5cd7
Tommy Duek <taduek@gmail.com>
2014-02-11 16:34:06 -0500

    Change skeleton controllers to use associations, rm unnecesary helper files and move auth to app_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_filter :require_current_user!, :only => [:show]
+  before_filter :require_current_user!, :only => [:index, :show]
   before_filter :require_no_current_user!, :only => [:create, :new]
 
   def create
     @user = User.new(params[:user])
 
     if @user.save
-      self.current_user = @user
+      sign_in(@user)
       redirect_to user_url(@user)
     else
       render :json => @user.errors.full_messages
@@ -18,10 +18,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    if params.include?(:id)
-      @user = User.find(params[:id])
-    else
+    if !params.include?(:id)
       redirect_to user_url(current_user)
+      return
     end
+
+    @user = User.find(params[:id])
   end
 end
+
